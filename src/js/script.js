daa2f69a355a6b9ec5d6b177310a35df23627a4d
unknown <joannakolbe@wp.pl>
2025-08-23 20:21:56 +0200

    Dodano metodę initAccordion w klasie Product, umożliwiającą zwijanie i rozwijanie produktów w formie akordeonu

--- src/js/script.js
+++ src/js/script.js
@@ -84,25 +84,26 @@
       menuContainer.appendChild(thisProduct.element);
     }
 
-    initAccordion() {
+    initAccordion(){
       const thisProduct = this;
 
-      // find clickable element (product header)
+      /* find the clickable trigger (the element that should react to clicking) */
       const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
-      // add event listener for click
-      clickableTrigger.addEventListener('click', function (event) {
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+        /* prevent default action for event */
         event.preventDefault();
 
-        // close other products (remove "active" class)
-        const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
-        for (let activeProduct of activeProducts) {
-          if (activeProduct !== thisProduct.element) {
-            activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
-          }
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct && activeProduct !== thisProduct.element) {
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
         }
 
-        // toggle "active" class on the clicked product
+        /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
     }
@@ -121,7 +122,6 @@
       console.log('app.initMenu');
       console.log('thisApp.data:', thisApp.data);
 
-      // create new Product instances for all products in data
       for (let productId in thisApp.data.products) {
         new Product(productId, thisApp.data.products[productId]);
       }
@@ -129,7 +129,6 @@
 
     init: function(){
       const thisApp = this;
-
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
@@ -143,6 +142,7 @@
 
   app.init();
 }
+
 
 
 
