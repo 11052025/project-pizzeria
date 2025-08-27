6ba374396fe8dcced730f37411b208434ec977bb
unknown <joannakolbe@wp.pl>
2025-08-27 21:42:39 +0200

    AmountWidget: dodano getElements() i podpięto w konstruktorze; integracja z Product

--- src/js/script.js
+++ src/js/script.js
@@ -61,27 +61,45 @@
       thisProduct.id = id;
       thisProduct.data = data;
 
+      // 1) render product HTML and insert into the DOM
       thisProduct.renderInMenu();
-      thisProduct.getElements();
-      thisProduct.initAccordion();
-      thisProduct.initOrderForm();
-      thisProduct.initAmountWidget(); // <-- initialize amount widget
-      thisProduct.processOrder();
 
-      // console.log('new Product:', thisProduct);
+      // 2) cache frequently used DOM elements inside this instance
+      thisProduct.getElements();
+
+      // 3) set up accordion behavior
+      thisProduct.initAccordion();
+
+      // 4) set up order form listeners
+      thisProduct.initOrderForm();
+
+      // 5) create and wire up amount widget
+      thisProduct.initAmountWidget();
+
+      // 6) initial processing of order (price & images)
+      thisProduct.processOrder();
     }
 
     renderInMenu() {
       const thisProduct = this;
+
+      // Generate HTML code based on template
       const generatedHTML = templates.menuProduct(thisProduct.data);
+
+      // Create DOM element from generated HTML
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      // Find menu container on the page
       const menuContainer = document.querySelector(select.containerOf.menu);
+
+      // Append newly created element to menu container
       menuContainer.appendChild(thisProduct.element);
     }
 
     getElements() {
       const thisProduct = this;
 
+      // Store references to important DOM nodes inside the product
       thisProduct.accordionTrigger =
         thisProduct.element.querySelector(select.menuProduct.clickable);
       thisProduct.form =
@@ -94,8 +112,6 @@
         thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper =
         thisProduct.element.querySelector(select.menuProduct.imageWrapper);
-
-      // reference to amount widget wrapper
       thisProduct.amountWidgetElem =
         thisProduct.element.querySelector(select.menuProduct.amountWidget);
     }
@@ -103,14 +119,21 @@
     initAccordion() {
       const thisProduct = this;
 
+      // Listen for clicks on the product header
       thisProduct.accordionTrigger.addEventListener('click', function (event) {
         event.preventDefault();
+
+        // Find currently active product (if any)
         const activeProduct = document.querySelector(
           select.all.menuProductsActive
         );
+
+        // If there is an active product and it's not this one, close it
         if (activeProduct && activeProduct !== thisProduct.element) {
           activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
         }
+
+        // Toggle this product
         thisProduct.element.classList.toggle(
           classNames.menuProduct.wrapperActive
         );
@@ -120,17 +143,20 @@
     initOrderForm() {
       const thisProduct = this;
 
+      // Handle form submit (Enter key)
       thisProduct.form.addEventListener('submit', function (event) {
         event.preventDefault();
         thisProduct.processOrder();
       });
 
+      // Handle any change in form inputs
       for (let input of thisProduct.formInputs) {
         input.addEventListener('change', function () {
           thisProduct.processOrder();
         });
       }
 
+      // Handle "Add to cart" button click
       thisProduct.cartButton.addEventListener('click', function (event) {
         event.preventDefault();
         thisProduct.processOrder();
@@ -140,34 +166,44 @@
     initAmountWidget() {
       const thisProduct = this;
 
-      // create new AmountWidget instance
+      // create new AmountWidget instance and keep a reference to it
       thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
     }
 
     processOrder() {
       const thisProduct = this;
+
+      // Convert form data into an object
       const formData = utils.serializeFormToObject(thisProduct.form);
 
+      // Start from base price
       let price = thisProduct.data.price;
 
+      // Iterate over all product parameters
       for (let paramId in thisProduct.data.params) {
         const param = thisProduct.data.params[paramId];
 
+        // Iterate over options within each parameter
         for (let optionId in param.options) {
           const option = param.options[optionId];
+
+          // Is this option selected in the form?
           const optionSelected =
             formData[paramId] && formData[paramId].includes(optionId);
 
+          // Adjust price based on selection and defaults
           if (optionSelected && !option.default) {
             price += option.price;
           } else if (!optionSelected && option.default) {
             price -= option.price;
           }
 
+          // Find image representing this param-option pair
           const optionImage = thisProduct.imageWrapper.querySelector(
             '.' + paramId + '-' + optionId
           );
 
+          // Show/hide image based on selection
           if (optionImage) {
             if (optionSelected) {
               optionImage.classList.add(classNames.menuProduct.imageVisible);
@@ -178,6 +214,7 @@
         }
       }
 
+      // Update price in the DOM (note: quantity multiplication will come later)
       thisProduct.priceElem.innerHTML = price;
     }
   }
@@ -185,10 +222,34 @@
   class AmountWidget {
     constructor(element) {
       const thisWidget = this;
-      thisWidget.element = element;
 
+      // Find and store widget elements
+      thisWidget.getElements(element);
+
+      // Debug logs (możesz zakomentować, gdy przetestujesz)
       console.log('AmountWidget:', thisWidget);
       console.log('constructor arguments:', element);
+    }
+
+    // Find input and +/- controls inside the widget wrapper
+    getElements(element) {
+      const thisWidget = this;
+
+      // Save wrapper element
+      thisWidget.element = element;
+
+      // Find input inside widget
+      thisWidget.input = thisWidget.element.querySelector(
+        select.widgets.amount.input
+      );
+
+      // Find "decrease" and "increase" links
+      thisWidget.linkDecrease = thisWidget.element.querySelector(
+        select.widgets.amount.linkDecrease
+      );
+      thisWidget.linkIncrease = thisWidget.element.querySelector(
+        select.widgets.amount.linkIncrease
+      );
     }
   }
 
@@ -215,6 +276,7 @@
 
   app.init();
 }
+
 
 
 
